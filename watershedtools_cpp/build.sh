15122882dd6f72730015791405b54cdc7bad61c9
Arthur MacKeith <amackeith@uchicago.edu>
2020-07-15 12:40:32 -0500

    build sm

--- watershedtools_cpp/build.sh
+++ watershedtools_cpp/build.sh
@@ -7,7 +7,7 @@
 
 # compile the C++ source code  -fPIC flag is needed for what we want to do
 
-gcc --std=c++11 -fPIC -c watershedtools_cpp.cpp
+g++ --std=c++11 -fPIC -c watershedtools_cpp.cpp
 
 # use SWIG on the interface file 'watershedtools_cpp.i'
 # this creates the wrapper
@@ -18,7 +18,7 @@ swig -python -c++ -o watershedtools_cpp_wrap.cpp watershedtools_cpp.i
 # then compile the wrapper, this step must link to the Python.h header as
 # well as the numpy header.
 # NOTE: you must have set up and activated the conda env before this step.
-gcc -std=c++11 -fPIC -c \
+g++ -std=c++11 -fPIC -c \
       -I "${CONDA_PREFIX}"/include/python3.6m \
       -I "${CONDA_PREFIX}"/lib/python3.6/site-packages/numpy/core/include \
         watershedtools_cpp_wrap.cpp
@@ -26,8 +26,9 @@ gcc -std=c++11 -fPIC -c \
 # Python will search for it under this name: watershedtools_cpp
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-gcc --std=c++11 -shared watershedtools_cpp.o watershedtools_cpp_wrap.o -o \
-      _watershedtools_cpp.so -lm
+g++ --std=c++11 -shared watershedtools_cpp.o watershedtools_cpp_wrap.o -o \
+      _watershedtools_cpp.so
+
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 ld -bundle -flat_namespace -undefined suppress -o _watershedtools_cpp.so \
       watershedtools_cpp.o watershedtools_cpp_wrap.o
